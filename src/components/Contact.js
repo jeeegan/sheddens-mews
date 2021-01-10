3c68a255610a603a39fd8d6949d83e917eb845c0
Jamie Egan <j.egan@mac.com>
2021-01-10 17:47:06 +0000

    form debugging

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,5 @@
 import React from 'react'
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
-
-const handleSubmit = e => {
-  e.preventDefault()
-  const form = e.target
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({
-      'form-name': form.getAttribute('name'),
-      ...this.state,
-    }),
-  })
-    .then(() => navigate(form.getAttribute('action')))
-    .catch(error => alert(error))
-}
-
 const Contact = props => (
   <section id="contact">
     <div className="inner">
@@ -29,7 +8,7 @@ const Contact = props => (
           method="POST"
           name="contact-form"
           data-netlify="true"
-          onSubmit={handleSubmit}
+        
         >
           <div className="field half first">
             <label htmlFor="name">Name</label>
@@ -43,7 +22,7 @@ const Contact = props => (
             <label htmlFor="message">Message</label>
             <textarea name="message" id="message" rows="6" required></textarea>
           </div>
-          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="form-name" value="contact-form" />
           <ul className="actions">
             <li>
               <input type="submit" value="Send Message" className="special" />
