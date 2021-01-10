852b87c64bad32ab45b6383b10d7c498891a7990
Jamie Egan <j.egan@mac.com>
2021-01-10 17:32:46 +0000

    more form changes...

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,10 @@
 import React from 'react'
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
-
 const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="POST" name="contact-form" data-netlify="true" onSubmit={handleSubmit}>
+                <form method="POST" name="contact-form" data-netlify="true">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" required/>
@@ -32,6 +17,7 @@ const Contact = (props) => (
                         <label htmlFor="message">Message</label>
                         <textarea name="message" id="message" rows="6" required></textarea>
                     </div>
+                    <input type="hidden" name="form-name" value="contact" />
                     <ul className="actions">
                         <li><input type="submit" value="Send Message" className="special" /></li>
                         <li><input type="reset" value="Clear" /></li>
