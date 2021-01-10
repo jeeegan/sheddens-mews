82f310cc9ac4c64668ab088cc0929cda72f07006
Jamie Egan <j.egan@mac.com>
2021-01-10 17:26:57 +0000

    debugging contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,36 @@
 import React from 'react'
 
+handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
 const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="POST" name="contact-form" data-netlify="true">
+                <form method="POST" name="contact-form" data-netlify="true" onSubmit={this.handleSubmit}>
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
+                        <input type="text" name="name" id="name" required/>
                     </div>
                     <div className="field half">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
+                        <input type="email" name="email" id="email" required/>
                     </div>
                     <div className="field">
                         <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
+                        <textarea name="message" id="message" rows="6" required></textarea>
                     </div>
                     <ul className="actions">
                         <li><input type="submit" value="Send Message" className="special" /></li>
